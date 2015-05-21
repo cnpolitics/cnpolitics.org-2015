4263a3e6c2375a210da0a75ff08c99703f57e309
Zhengyu Qian <laerhsif@gmail.com>
2015-05-22 06:38:34 +0800

    Add the script of the homepage (`home.js`).

---
+++ assets/home-min.js
@@ -0,0 +1,1 @@
+!function(){$(".db-index .switch").click(function(){$(".db-index").toggleClass("active"),console.log("test!")})}();
---
+++ javascripts/home.js
@@ -0,0 +1,8 @@
+;(function() {
+	
+$('.db-index .switch').click(function() {
+	$('.db-index').toggleClass('active');
+	console.log('test!');
+});
+	
+}());
